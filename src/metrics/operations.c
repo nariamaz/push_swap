1b2471f84211fad4433a81feed237b89decef312
nariamaz <marianasantossales@gmail.com>
2026-09-04 13:39:47 -0300

    refactor!: organizes header structs and extract parsing, validation and stack filling from main

--- src/metrics/operations.c
+++ src/metrics/operations.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maridos- <maridos-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: maridos- <maridos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 17:13:40 by maridos-          #+#    #+#             */
-/*   Updated: 2026/09/01 21:57:47 by maridos-         ###   ########.fr       */
+/*   Updated: 2026/09/04 13:24:16 by maridos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h" 
+#include "push_swap.h"
 
-void ft_print_counts(t_metrics *metrics)
+void ft_print_counts(t_data data)
 {
     const char *op_names[] = {
-    "sa", "sb", "ss", "pa", "pb", "ra", 
+    "sa", "sb", "ss", "pa", "pb", "ra",
     "rb", "rr","rra", "rrb", "rrr"
     };
     int i;
@@ -26,7 +26,7 @@ void ft_print_counts(t_metrics *metrics)
     {
         ft_putstr_fd((char *)op_names[i], 2);
         ft_putstr_fd(": ", 2);
-        ft_putnbr_fd(metrics->op_counter[i], 2);
+        ft_putnbr_fd(data.op_counter[i], 2);
         if (i == PB)
         {
             ft_putchar_fd('\n', 2);
@@ -39,16 +39,16 @@ void ft_print_counts(t_metrics *metrics)
     ft_putchar_fd('\n', 2);
 }
 
-int ft_total_ops(t_metrics metrics)
+int ft_total_ops(t_data data)
 {
     int i;
     int sum;
-    
+
     i = 0;
     sum = 0;
     while (i < OP_COUNT)
     {
-        sum = sum + metrics.op_counter[i];
+        sum = sum + data.op_counter[i];
         i++;
     }
     return (sum);
